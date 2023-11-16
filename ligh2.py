21a147740a45ba463b130d8ebe829f6f068a8909
unknown <alejandro7899871776@gmail.com>
2023-11-16 01:22:21 -0500

    adding streaming and chat names

--- ligh2.py
+++ ligh2.py
@@ -7,7 +7,7 @@ import uuid
 import pickle
 from openai import OpenAI
 import os 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = ""
 client = OpenAI()
 
 
@@ -17,7 +17,34 @@ avatar_user = Image.open(r"utils\logo_white_icon.png")
 
 
 
-def chat_message(messages,temperature = 0.5):
+
+def chat_titles(messages):
+    user_content = ""
+    for message in messages:
+        if message['role'] == 'user':
+            user_content += message['content']
+    if user_content != "":
+        promt = f'''- create a conversation thumbail/topic based on the following user request {user_content}
+                    super short 
+                    - example : for a conversation about python and flask thumbnail should be :  "python flask implementation."
+                    - your output should be only the title/thumbnail/topic 
+                    - your answerr will be used to name a file so do not use sybbols semi colon and so on'''
+        
+        params = chat_message([HumanMessage(promt)],temperature = 0,stream=False)
+        _thumbnail = client.chat.completions.create(**params)
+        thumbnail = _thumbnail.choices[0].message.content
+    else:
+        thumbnail = "empty chat"
+    return thumbnail
+
+    
+
+
+
+
+
+
+def chat_message(messages,temperature = 0.5,stream = True):
         
 
         return {
@@ -25,7 +52,8 @@ def chat_message(messages,temperature = 0.5):
             "messages": messages,
             
             "max_tokens": 4096,
-            "temperature": temperature
+            "temperature": temperature,
+            "stream":stream
         }
 
 
@@ -93,10 +121,20 @@ def init_messages(content = _content):
 def save_conversation():
     """Saves the current conversation using pickle with a unique filename."""
     if "messages" in st.session_state:
-        filename = f"conversations/{uuid.uuid4()}.pkl"
+        # try:
+        thumbnail = chat_titles(st.session_state.messages)
+        # thumbnail = result.choices[0].message.content
+        print(thumbnail )
+        filename = f"conversations/{thumbnail}.pkl"
         with open(filename, 'wb') as file:
             pickle.dump(st.session_state.messages, file)
         return filename
+        # except:
+        #     filename = f"conversations/{uuid.uuid4()}.pkl"
+        #     with open(filename, 'wb') as file:
+        #         pickle.dump(st.session_state.messages, file)
+        #     return filename
+
     return None
 
 def load_conversation(filename):
@@ -111,27 +149,23 @@ def display_saved_conversations():
         if st.sidebar.button(file):
             st.session_state.messages = load_conversation(f"conversations/{file}")
 
-            
+
+def get_last_user_message():
+    """
+    Function to retrieve the last message sent by a user.
+    """
+    messages = st.session_state.get("messages", [])
+    for message in reversed(messages):
+        if message['role'] == 'user':
+            return message['content']
+    return None  # Return None if no user message is found
+
+
 
 def main():
     init_messages()
     temperature = select_model()
     display_saved_conversations()
-    # Supervise user input
-    if user_input := st.chat_input("Input your question!"):
-        st.session_state.messages.append( HumanMessage(user_input))
-        with st.spinner("MinckaGPT is typing ..."):
-            
-            params = chat_message(messages=st.session_state.messages,temperature=temperature)
-
-            result = client.chat.completions.create(**params)
-            answer =result.choices[0].message.content
-            print("result.choices[0].message" , ":" , answer)
-
-                # st.session_state.costs.append(cost)
-        st.session_state.messages.append(AIMessage(answer))
-        
-
 
     # Display chat history
     messages = st.session_state.get("messages", [])
@@ -141,19 +175,40 @@ def main():
                 print(message)
                 st.markdown(message['content'])
         
-        elif message['role'] == 'user':
+        if message['role'] == 'user':
             with st.chat_message("user", avatar=avatar_user):
                 
                 st.markdown(message['content'] )
+
+    if user_input := st.chat_input("Input your question!"):
+        st.session_state.messages.append(HumanMessage(user_input))
+        with st.chat_message("user", avatar=avatar_user):
+            st.markdown(get_last_user_message())
+
+        params = chat_message(messages=st.session_state.messages,temperature=temperature)
+        with st.chat_message("assistant", avatar=avatar_bot):
+            text_placeholder = st.empty()
+
+            collected_messages = ""
+            result = client.chat.completions.create(**params)
+            for chunks in result:
+                chunk_message = chunks.choices[0].delta.content
+                if type(chunk_message) == str:
+                    collected_messages += chunk_message
+                    # st.markdown(collected_messages)
+                    text_placeholder.markdown(collected_messages)
+                else:
+                    pass
+
+            # answer =result.choices[0].message.content
+                answer = collected_messages
+                print("result.choices[0].message" , ":" , answer)
+
+
+        st.session_state.messages.append(AIMessage(answer))
         
 
-             
-        # if isinstance(message, AIMessage):
-        #     with st.chat_message("assistant", avatar=avatar_bot):
-        #         st.markdown(message.content)
-        # elif isinstance(message, HumanMessage):
-        #     with st.chat_message("user", avatar=avatar_user):
-        #         st.markdown(message.content)
+
 
 if not os.path.exists('conversations'):
     os.makedirs('conversations')
